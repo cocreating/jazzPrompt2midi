71d5fdc445eb8d8ddf7db07c8b58eaca9d7b57bb
Jaume Subirats <jaume.subirats@gmail.com>
2026-03-02 00:46:27 +0100

    feat: add Bright Piano and Electric Piano instruments, fix vite glob warning

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,11 @@ import * as Tone from 'tone';
 import VirtualPiano from './lib/VirtualPiano.svelte';
 
 // Load default payloads from assets folder dynamically
-const rawDefaults = import.meta.glob('./assets/defaults/*.txt', { eager: true, as: 'raw' });
+const rawDefaults = import.meta.glob('./assets/defaults/*.txt', { eager: true, query: '?raw', import: 'default' });
 
 const defaultsMap = Object.entries(rawDefaults).reduce((acc, [path, content]) => {
     const filename = path.split('/').pop();
-    const fileContent = content;
+    const fileContent = String(content);
     const titleMatch = fileContent.match(/TITLE:\s*(.+)/i);
     const title = titleMatch ? titleMatch[1].trim() : filename;
 
@@ -141,7 +141,7 @@ const clearActiveNotes = () => {
     activeMelodyNotes = new Set();
 };
 
-let instrument = $state('piano');
+let instrument = $state('piano'); // piano, bright-piano, electric-piano, guitar
 let voicingMode = $state('jazz');
 let zenMode = $state(false);
 
@@ -300,29 +300,64 @@ const togglePlay = async () => {
             A7: "A7.mp3", C8: "C8.mp3"
         };
 
-        const guitarUrls = {
-            "F#2": "Fs2.mp3", "F#3": "Fs3.mp3", "F#4": "Fs4.mp3", "F#5": "Fs5.mp3",
-            G2: "G2.mp3", G3: "G3.mp3", G4: "G4.mp3", G5: "G5.mp3",
-            "G#2": "Gs2.mp3", "G#3": "Gs3.mp3", "G#4": "Gs4.mp3", "G#5": "Gs5.mp3",
-            A2: "A2.mp3", A3: "A3.mp3", A4: "A4.mp3", A5: "A5.mp3",
-            "A#2": "As2.mp3", "A#3": "As3.mp3", "A#4": "As4.mp3", "A#5": "As5.mp3",
-            B2: "B2.mp3", B3: "B3.mp3", B4: "B4.mp3", B5: "B5.mp3",
-            C3: "C3.mp3", C4: "C4.mp3", C5: "C5.mp3", C6: "C6.mp3",
-            "C#3": "Cs3.mp3", "C#4": "Cs4.mp3", "C#5": "Cs5.mp3", "C#6": "Cs6.mp3",
-            D3: "D3.mp3", D4: "D4.mp3", D5: "D5.mp3", D6: "D6.mp3",
-            "D#3": "Ds3.mp3", "D#4": "Ds4.mp3", "D#5": "Ds5.mp3", "D#6": "Ds6.mp3",
-            E2: "E2.mp3", E3: "E3.mp3", E4: "E4.mp3", E5: "E5.mp3"
+        const selectedUrls = {
+            piano: {
+                A0: "A0.mp3", C1: "C1.mp3", "D#1": "Ds1.mp3", "F#1": "Fs1.mp3",
+                A1: "A1.mp3", C2: "C2.mp3", "D#2": "Ds2.mp3", "F#2": "Fs2.mp3",
+                A2: "A2.mp3", C3: "C3.mp3", "D#3": "Ds3.mp3", "F#3": "Fs3.mp3",
+                A3: "A3.mp3", C4: "C4.mp3", "D#4": "Ds4.mp3", "F#4": "Fs4.mp3",
+                A4: "A4.mp3", C5: "C5.mp3", "D#5": "Ds5.mp3", "F#5": "Fs5.mp3",
+                A5: "A5.mp3", C6: "C6.mp3", "D#6": "Ds6.mp3", "F#6": "Fs6.mp3",
+                A6: "A6.mp3", C7: "C7.mp3", "D#7": "Ds7.mp3", "F#7": "Fs7.mp3",
+                A7: "A7.mp3", C8: "C8.mp3"
+            },
+            'bright-piano': {
+                A0: "A0.mp3", C1: "C1.mp3", "D#1": "Ds1.mp3", "F#1": "Fs1.mp3",
+                A1: "A1.mp3", C2: "C2.mp3", "D#2": "Ds2.mp3", "F#2": "Fs2.mp3",
+                A2: "A2.mp3", C3: "C3.mp3", "D#3": "Ds3.mp3", "F#3": "Fs3.mp3",
+                A3: "A3.mp3", C4: "C4.mp3", "D#4": "Ds4.mp3", "F#4": "Fs4.mp3",
+                A4: "A4.mp3", C5: "C5.mp3", "D#5": "Ds5.mp3", "F#5": "Fs5.mp3",
+                A5: "A5.mp3", C6: "C6.mp3", "D#6": "Ds6.mp3", "F#6": "Fs6.mp3",
+                A6: "A6.mp3", C7: "C7.mp3", "D#7": "Ds7.mp3", "F#7": "Fs7.mp3",
+                A7: "A7.mp3", C8: "C8.mp3"
+            },
+            'electric-piano': {
+                A1: "A1.mp3", C2: "C2.mp3", "D#2": "Ds2.mp3", "F#2": "Fs2.mp3",
+                A2: "A2.mp3", C3: "C3.mp3", "D#3": "Ds3.mp3", "F#3": "Fs3.mp3",
+                A3: "A3.mp3", C4: "C4.mp3", "D#4": "Ds4.mp3", "F#4": "Fs4.mp3",
+                A4: "A4.mp3", C5: "C5.mp3", "D#5": "Ds5.mp3", "F#5": "Fs5.mp3",
+                A5: "A5.mp3", C6: "C6.mp3", "D#6": "Ds6.mp3", "F#6": "Fs6.mp3",
+                A6: "A6.mp3", C7: "C7.mp3", "D#7": "Ds7.mp3", "F#7": "Fs7.mp3"
+            },
+            guitar: {
+                "F#2": "Fs2.mp3", "F#3": "Fs3.mp3", "F#4": "Fs4.mp3", "F#5": "Fs5.mp3",
+                G2: "G2.mp3", G3: "G3.mp3", G4: "G4.mp3", G5: "G5.mp3",
+                "G#2": "Gs2.mp3", "G#3": "Gs3.mp3", "G#4": "Gs4.mp3", "G#5": "Gs5.mp3",
+                A2: "A2.mp3", A3: "A3.mp3", A4: "A4.mp3", A5: "A5.mp3",
+                "A#2": "As2.mp3", "A#3": "As3.mp3", "A#4": "As4.mp3", "A#5": "As5.mp3",
+                B2: "B2.mp3", B3: "B3.mp3", B4: "B4.mp3", B5: "B5.mp3",
+                C3: "C3.mp3", C4: "C4.mp3", C5: "C5.mp3", C6: "C6.mp3",
+                "C#3": "Cs3.mp3", "C#4": "Cs4.mp3", "C#5": "Cs5.mp3", "C#6": "Cs6.mp3",
+                D3: "D3.mp3", D4: "D4.mp3", D5: "D5.mp3", D6: "D6.mp3",
+                "D#3": "Ds3.mp3", "D#4": "Ds4.mp3", "D#5": "Ds5.mp3", "D#6": "Ds6.mp3",
+                E2: "E2.mp3", E3: "E3.mp3", E4: "E4.mp3", E5: "E5.mp3"
+            }
         };
 
-        const selectedUrls = instrument === 'piano' ? pianoUrls : guitarUrls;
-        const selectedBase = instrument === 'piano'
-            ? "https://tonejs.github.io/audio/salamander/"
-            : "https://nbrosowsky.github.io/tonejs-instruments/samples/guitar-acoustic/";
+        const baseUrls = {
+            piano: "https://tonejs.github.io/audio/salamander/",
+            'bright-piano': "https://nbrosowsky.github.io/tonejs-instruments/samples/piano/",
+            'electric-piano': "https://raw.githubusercontent.com/Alexander-T-S/Tone.js-Instruments/master/samples/piano-electric/",
+            guitar: "https://nbrosowsky.github.io/tonejs-instruments/samples/guitar-acoustic/"
+        };
+
+        const selectedUrlsMap = selectedUrls[instrument] || selectedUrls.piano;
+        const selectedBase = baseUrls[instrument] || baseUrls.piano;
 
         statusText = `Downloading ${instrument} Samples...`;
         try {
             sampler = new Tone.Sampler({
-                urls: selectedUrls,
+                urls: selectedUrlsMap,
                 release: 1,
                 baseUrl: selectedBase
             }).toDestination();
@@ -581,9 +616,11 @@ const toggleOptions = () => {
 
             <div class="option-group">
                 <label for="instr-select">Instrument</label>
-                <select id="instr-select" bind:value={instrument} onchange={() => { if(sampler) { sampler.dispose(); sampler = null; } }}>
-                    <option value="piano">Salamander Piano</option>
-                    <option value="guitar">Acoustic Guitar</option>
+                <select id="instr-select" bind:value={instrument} onchange={() => { stopSequence(); if(sampler) { sampler.dispose(); sampler = null; } }}>
+                    <option value="piano">Classic Grand (Salamander)</option>
+                    <option value="bright-piano">Bright Piano (VSO2)</option>
+                    <option value="electric-piano">Electric Piano (Rhodes)</option>
+                    <option value="guitar">Jazz Guitar (Acoustic)</option>
                 </select>
             </div>
 
